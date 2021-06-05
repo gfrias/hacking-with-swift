effb1fb272a58b2392991aa69220773187d6e71a
Guillermo Frias <guillef1@gmail.com>
2021-06-05 12:37:54 +0200

    BetterRest

---
+++ 100-days-swift-ui/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Guillermo Frias on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
